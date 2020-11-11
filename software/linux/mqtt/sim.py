1ae0155065d6e3f995b7768798ff5996e452fcc5
tuannv <tuanngo0898@gmail.com>
2020-11-11 16:22:29 +0700

    Able to show node type and location in node list

--- software/linux/mqtt/sim.py
+++ software/linux/mqtt/sim.py
@@ -5,6 +5,8 @@ import time
 
 serial = Serial(config.SERIAL_PORT_SIM, config.SERIAL_BAUD_SIM)
 
+NODE_MAX = 10
+
 def loop_write():
     address = 0
     location_x = 123.456
@@ -12,17 +14,20 @@ def loop_write():
     location_z = 789.123
     cnt = 0
     while True:
-        str = '{"type": "STATUS", "msg": {"address": "%s", "type": "ANCHOR", "location": [%f, %f, %f]}}\n' % (address, location_x, location_y, location_z)
-        serial.write(bytes(str, 'utf-8'))
-        time.sleep(0.5)
-
+        if(address < NODE_MAX/2):
+            str = '{"type": "STATUS", "msg": {"address": "%s", "type": "ANCHOR", "location": [%f, %f, %f]}}\n' % (address, location_x, location_y, location_z)
+            serial.write(bytes(str, 'utf-8'))
+        else:
+            str = '{"type": "STATUS", "msg": {"address": "%s", "type": "TAG", "location": [%f, %f, %f]}}\n' % (address, location_x, location_y, location_z)
+            serial.write(bytes(str, 'utf-8'))
+        time.sleep(0.05)
         location_x += 1
         location_y += 2
         location_z += 10
         cnt += 1
         if(not (cnt%20)):
             address += 1
-            address %= 2
+            address = address % NODE_MAX
 
 def loop_read():
     while True:
