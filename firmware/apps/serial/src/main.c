3039ed8b38b0b884121c80430fced6307fcdc6f7
tuannv <tuanngo0898@gmail.com>
2020-11-14 13:42:45 +0700

    add serial lib and app

---
+++ firmware/apps/serial/src/main.c
@@ -0,0 +1,38 @@
+#include <os/mynewt.h>
+#include <serial/serial.h>
+
+#define STACK_SIZE  128
+#define PRIORITY    101
+
+struct os_task task;           
+os_stack_t task_stack[STACK_SIZE];
+
+static void
+task_func(void *arg)
+{
+    char chr[2];
+    chr[1] = 0;
+    while(1){
+        chr[0] = serial_read();
+        printf("%s", chr);
+        serial_write(chr, 1);
+    }
+}
+
+int main(int argc, char **argv){
+    sysinit();
+
+    printf("App: gateway\n");
+
+    os_task_init(&task, "echoback",
+                task_func,
+                NULL,
+                PRIORITY, 
+                OS_WAIT_FOREVER,
+                task_stack,
+                STACK_SIZE);
+
+    while (1) {
+        os_eventq_run(os_eventq_dflt_get());
+    }
+}
