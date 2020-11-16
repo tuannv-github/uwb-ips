a334df5c5b376d21e5f48f1fca763710626a35ef
tuannv <tuanngo0898@gmail.com>
2020-11-16 20:39:04 +0700

    Fix message parse

--- firmware/lib/message/src/mesh_msg.c
+++ firmware/lib/message/src/mesh_msg.c
@@ -12,7 +12,7 @@ void msg_prepr_rtls(struct os_mbuf *mbuf, msg_rtls_t *msg){
     switch (msg->msg_type)
     {
     case MAVLINK_MSG_ID_LOCATION:
-        net_buf_simple_add_u8(mbuf, msg->msg_type | msg->node_type << 4);
+        net_buf_simple_add_u8(mbuf, (msg->msg_type & 0x0F) | (msg->node_type << 4));
         net_buf_simple_add_be16(mbuf, msg->dstsrc);
         net_buf_simple_add_be32(mbuf, *((uint32_t *)(&msg->location_x)));
         net_buf_simple_add_be32(mbuf, *((uint32_t *)(&msg->location_y)));
@@ -29,12 +29,13 @@ void msg_prepr_rtls(struct os_mbuf *mbuf, msg_rtls_t *msg){
 }
 
 void msg_parse_rtls(struct os_mbuf *mbuf, msg_rtls_t *msg){
-    msg->msg_type = net_buf_simple_pull_u8(mbuf) & 0x0F;
+    uint8_t type = net_buf_simple_pull_u8(mbuf);
+    msg->msg_type = type & 0x0F;
     msg->dstsrc = net_buf_simple_pull_be16(mbuf);
     switch (msg->msg_type)
     {
     case MAVLINK_MSG_ID_LOCATION:
-        msg->node_type = (msg->msg_type & 0xF0) >> 4;
+        msg->node_type = type >> 4;
         *((uint32_t *)(&msg->location_x)) = net_buf_simple_pull_be32(mbuf);
         *((uint32_t *)(&msg->location_y)) = net_buf_simple_pull_be32(mbuf);
         *((uint32_t *)(&msg->location_z)) = net_buf_simple_pull_be32(mbuf);
