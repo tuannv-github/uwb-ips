7f6c37c41543756b6363295b23f062945a08a21d
tuannv <tuanngo0898@gmail.com>
2020-11-27 21:52:54 +0700

    add analysis code

--- software/algorithm/subscriber.py
+++ software/algorithm/subscriber.py
@@ -1,16 +1,23 @@
 import paho.mqtt.client as mqtt
 import json
 
+DISTANCE_M = "1.5"
+FILE_NAME = "distance/" + DISTANCE_M
+
 client = mqtt.Client()
+f = open(FILE_NAME, "w")
+count = 0
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe("topic_evt")
 
 def on_message(client, userdata, msg):
+    global count
     msg = json.loads(msg.payload.decode("utf-8"))
-    if(msg["mavpackettype"] == "TOF"):
-        if(msg["anchor"] == 3999):
-            print(msg)
+    if(msg["mavpackettype"] == 'TOF'):
+        f.write(str(msg["tof"]) + "\n")
+        count+=1
+        print(str(count) + ": " + str(msg))
 
 client.on_connect = on_connect
 client.on_message = on_message
