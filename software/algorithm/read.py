cfbdecf5ad3b1c0d9ce02b0a969ee73d77586cae
tuannv <tuanngo0898@gmail.com>
2020-11-30 00:26:30 +0700

    add docs

--- software/algorithm/read.py
+++ software/algorithm/read.py
@@ -6,28 +6,41 @@ def trilaterate3D(distances):
     p2=np.array(distances[1][:3])
     p3=np.array(distances[2][:3])       
     p4=np.array(distances[3][:3])
-    r1=distances[0][-1]
-    r2=distances[1][-1]
-    r3=distances[2][-1]
-    r4=distances[3][-1]
-    e_x=(p2-p1)/np.linalg.norm(p2-p1)
-    i=np.dot(e_x,(p3-p1))
-    e_y=(p3-p1-(i*e_x))/(np.linalg.norm(p3-p1-(i*e_x)))
-    e_z=np.cross(e_x,e_y)
-    d=np.linalg.norm(p2-p1)
-    j=np.dot(e_y,(p3-p1))
-    x=((r1**2)-(r2**2)+(d**2))/(2*d)
-    y=(((r1**2)-(r3**2)+(i**2)+(j**2))/(2*j))-((i/j)*(x))
-    z1=np.sqrt(r1**2-x**2-y**2)
-    z2=np.sqrt(r1**2-x**2-y**2)*(-1)
-    ans1=p1+(x*e_x)+(y*e_y)+(z1*e_z)
-    ans2=p1+(x*e_x)+(y*e_y)+(z2*e_z)
-    dist1=np.linalg.norm(p4-ans1)
-    dist2=np.linalg.norm(p4-ans2)
-    if np.abs(r4-dist1)<np.abs(r4-dist2):
-        return ans1
-    else: 
-        return ans2
+    # r1=distances[0][-1]
+    # r2=distances[1][-1]
+    # r3=distances[2][-1]
+    # r4=distances[3][-1]
+    
+    d1=distances[0][-1]
+    d2=distances[1][-1]
+    d3=distances[2][-1]
+    d4=distances[3][-1]
+
+    pt1 = np.array([0,0,0])
+    pt2 = p2 - p1
+    pt3 = p3 - p1
+    pt4 = p4 - p1
+    
+    return [0, 0 ,0]
+
+    # e_x=(p2-p1)/np.linalg.norm(p2-p1)
+    # i=np.dot(e_x,(p3-p1))
+    # e_y=(p3-p1-(i*e_x))/(np.linalg.norm(p3-p1-(i*e_x)))
+    # e_z=np.cross(e_x,e_y)
+    # d=np.linalg.norm(p2-p1)
+    # j=np.dot(e_y,(p3-p1))
+    # x=((r1**2)-(r2**2)+(d**2))/(2*d)
+    # y=(((r1**2)-(r3**2)+(i**2)+(j**2))/(2*j))-((i/j)*(x))
+    # z1=np.sqrt(r1**2-x**2-y**2)
+    # z2=np.sqrt(r1**2-x**2-y**2)*(-1)
+    # ans1=p1+(x*e_x)+(y*e_y)+(z1*e_z)
+    # ans2=p1+(x*e_x)+(y*e_y)+(z2*e_z)
+    # dist1=np.linalg.norm(p4-ans1)
+    # dist2=np.linalg.norm(p4-ans2)
+    # if np.abs(r4-dist1)<np.abs(r4-dist2):
+    #     return ans1
+    # else: 
+    #     return ans2
 
 read = open("dataset.txt", "r")
 write = open("location.txt", "w")
